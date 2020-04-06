e757bac98bb3734c398d48cec08d074de0327148
Michał Pawełczyk <michal@localhost.localdomain>
2020-04-06 17:19:27 +0200

    initial commit with example spring 5 project

---
+++ src/main/java/pro/pawelczyk/spring5initialprojectexample/Spring5InitialProjectExampleApplication.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.spring5initialprojectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5InitialProjectExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5InitialProjectExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/pawelczyk/spring5initialprojectexample/Spring5InitialProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.spring5initialprojectexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5InitialProjectExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
